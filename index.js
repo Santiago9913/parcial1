356746904888cf5b8f433af4efce8477cc677ddc
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2022-03-15 13:50:00 -0500

    primera

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let foodCard = `<div class="card" style="width: 18rem;">
 <img src="{{image}}" class="card-img-top" alt="...">
 <div class="card-body">
   <h5 class="card-title">{{name}}</h5>
-  <p class="card-text"><b>{{description}}</b></p>
+  <p class="card-text">{{description}}</p>
   <p class="card-text"><b>{{price}}</b></p>
   <a class="btn btn-warning">Add to cart</a>
 </div>
